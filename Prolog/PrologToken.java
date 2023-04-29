6a429ec76cc35fa3d0aa6a54fd84d04948250964
MundatiC <calebngethe230@gmail.com>
2023-04-29 18:31:17 +0300

    added new folder

---
+++ Prolog/PrologToken.java
@@ -0,0 +1,18 @@
+// Output created by jacc on Sat Apr 29 17:41:29 EAT 2023
+
+interface PrologToken {
+    int ENDINPUT = 0;
+    int CHAR = 1;
+    int CHARR = 2;
+    int INTEGER = 3;
+    int SYMBOLS = 4;
+    int error = 5;
+    // '"' (code=34)
+    // '(' (code=40)
+    // ')' (code=41)
+    // ',' (code=44)
+    // '-' (code=45)
+    // '.' (code=46)
+    // ':' (code=58)
+    // '?' (code=63)
+}
---
+++ Prolog/README.md
@@ -0,0 +1,22 @@
+# Parser
+This is a simple prolog parser in jacc
+## Installation
+Clone this repo
+Navigate to the directory
+Type this command
+```bash
+java Prolog
+```
+## Usage
+
+```bash
+jacc prolog.jacc
+
+javac *.java
+
+java Prolog
+```
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
